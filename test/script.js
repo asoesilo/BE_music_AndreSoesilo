830d4dfca801d51563854fd990f71ad0e1f1b616
Andre Soesilo <asoesilo84@gmail.com>
2014-07-09 13:57:50 -0700

    Add comment to test script

--- test/script.js
+++ test/script.js
@@ -5,18 +5,24 @@ var follows = require('./helpers/load_follows');
 var listen = require('./helpers/load_listen');
 
 describe('Script', function() {
+  // Loads the follows.json and listen.json
   before(function(done) {
+    // Loads the list of followers and followees
     follows.load(function(err) {
       if(err) {
         return done(err);
       }
+
+      // Loads the list of music that users has listened to
       listen.load(done);
     });
   });
 
+  // Get the list of music recommendation for user 'a'
   it('get recommended list of music', function(done) {
     var userID = 'a';
 
+    // Send out the GET /recommendations request
     request(app)
       .get('/recommendations')
       .send({user: userID})
@@ -25,6 +31,7 @@ describe('Script', function() {
           return done(err);
         }
 
+        // Display the recommended music list
         console.log("The recommended music list is:");
         console.log(req.body.list);
         done();
