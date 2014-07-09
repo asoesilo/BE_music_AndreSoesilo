92145877ac9f69c474cbba13654596274c890a63
Andre Soesilo <asoesilo84@gmail.com>
2014-07-08 21:11:28 -0700

    Fix comment in listen routes

--- routes/listen.js
+++ routes/listen.js
@@ -4,11 +4,11 @@ var musicLists = require('../app/controllers/music_lists');
 
 var utils = require('../lib/utils');
 
-// Parse 'from' and 'to' params
+// Parse 'user' and 'music' params
 router.use(utils.parseUserParams('user'));
 router.use(utils.parseMusicParams('music'));
 
-// POST /follow
+// POST /listen
 router.post('/', musicLists.add);
 
 module.exports = router;
